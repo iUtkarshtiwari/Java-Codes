9fd52078b227bb412987c8e3ae35bd8e68b0b1e7
Utkarsh <utkarshtiwari102005@gmail.com>
2024-01-21 11:44:39 +0530

    Java Starting

---
+++ number_is_integer_or_not.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class number_is_integer_or_not {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+}
