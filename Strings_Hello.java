7081bc857e55a48a3226ea9b11670f79377d915c
Utkarsh <utkarshtiwari102005@gmail.com>
2024-01-21 12:14:24 +0530

    Java String Starting

--- Strings_Hello.java
+++ Strings_Hello.java
@@ -3,6 +3,11 @@ public class Strings_Hello {
     public static void main(String[] args) {
         String name=new String("Hello");
         System.out.println(name);
+        System.out.print("Good Morning Everyone just say ");
+        System.out.println("Hello");
+        int a=5,b=10;
+        System.out.printf("Hello i am good i am %d years old and in next 5 years i will be %d years old.",a,b);
     }
+
     
 }
