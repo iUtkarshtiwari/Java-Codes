7cda2de35d7bcedf21a48758ac543b61f11da4dd
Utkarsh <utkarshtiwari102005@gmail.com>
2024-01-21 12:03:53 +0530

    Java String Starting

---
+++ Strings_Hello.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Strings_Hello {
+    public static void main(String[] args) {
+        String name=new String("Hello");
+        System.out.println(name);
+    }
+    
+}
