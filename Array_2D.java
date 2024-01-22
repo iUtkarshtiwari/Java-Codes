c6624e6b3f2520b8a37c5784a09c8c0904cf6998
Utkarsh <utkarshtiwari102005@gmail.com>
2024-01-22 16:29:39 +0530

    Java Using for loop

---
+++ Array_2D.java
@@ -0,0 +1,22 @@
+public class Array_2D {
+    public static void main(String[] args) {
+        int[] marks;   //1D array
+        int[][]flats;    //2D array
+        flats=new int[2][3];      //2d array of 2rows and 3 columns
+        flats[0][0]=101;
+        flats[0][1]=102;
+        flats[0][2]=103;
+        flats[1][0]=201;
+        flats[1][1]=202;
+        flats[1][2]=203;
+        for (int i = 0; i < flats.length; i++) {
+            for (int j = 0; j < flats[i].length; j++) {
+                
+                System.out.print(flats[i][j]);
+                System.out.print(" ");
+            }
+                System.out.println("");
+        }
+        
+    }
+}
