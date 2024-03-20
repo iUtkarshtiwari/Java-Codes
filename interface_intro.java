123c1145c6ed7270460da035d30075934d3150a0
Utkarsh <utkarshtiwari102005@gmail.com>
2024-03-20 15:57:47 +0530

    Use of Interface in java

--- interface_intro.java
+++ interface_intro.java
@@ -31,29 +31,71 @@
     
 // }
 
-interface Car {            
-    void breakSystem();    
-    void oilingSystem();    
-}
+// interface Car {            
+//     void breakSystem();    
+//     void oilingSystem();    
+// }
 
-class BMWs implements Car {
-    public void breakSystem() {
-        System.out.println("Every car should have a proper braking system");
-    }
+// class BMWs implements Car {
+//     public void breakSystem() {
+//         System.out.println("Every car should have a proper braking system");
+//     }
 
-    public void oilingSystem() {
-        System.out.println("Every car should have a proper oiling system");
-    }
+//     public void oilingSystem() {
+//         System.out.println("Every car should have a proper oiling system");
+//     }
 
-    public void powerSystem() {
-        System.out.println("Every car should have a power system");
-    }
-}
+//     public void powerSystem() {
+//         System.out.println("Every car should have a power system");
+//     }
+// }
 
-public class interface_intro {
-    public static void main(String[] args) {
-        BMWs b1 = new BMWs();
-        b1.powerSystem();
-        b1.oilingSystem();
-    }
+// public class interface_intro {
+//     public static void main(String[] args) {
+//         BMWs b1 = new BMWs();
+//         b1.powerSystem();
+//         b1.oilingSystem();
+//     }
+// }
+//////////////////////////////////////////////////////////////////
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+  interface Safety{
+      void safetyRule();
+  }
+  interface Car extends Safety{
+      void breakSystem();   // public 
+      void OilingSystem();  // public 
+  }
+    //private < protected < default < public
+ abstract class BMW implements Car{
+    
+      public void OilingSystem(){
+          System.out.println("i am oiling System of BMW");
+      }
+     public  void powerSystem(){
+          System.out.println(" i have extra power for race");
+      }
+     public void safetyRule(){
+         System.out.println("you have to follow rules");
+     }
+  }
+  class BWMchild extends BMW {
+      public void breakSystem(){
+          System.out.println("breaking system");
+      }
+  }
+  
+  
+ 
+
+class interface_intro {
+	public static void main (String[] args) {
+	BWMchild b1 = new BWMchild();
+	  b1.powerSystem();
+	  b1.breakSystem();
+		  
+	}
 }
